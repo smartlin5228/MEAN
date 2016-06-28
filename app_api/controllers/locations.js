4f9f5919b9b06bc1d9670991e47302f360113b62
smartlin5228 <smartlin1@gmail.com>
2016-06-28 02:07:14 -0700

    New api

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Locations'); // access the Location collection
+
+
+var sendJsonResponse = function (res, status, content) {
+	res.status(status);
+	res.json(content);
+};
+module.exports.locationsListByDistance =function (req, res) {
+	sendJsonResponse(res, 200, {"status": "success"});
+};
+module.exports.locationsCreate = function (req, res) {
+	sendJsonResponse(res, 200, {"status": "success"});
+};
+module.exports.locationsReadOne = function (req, res) {
+	if (req.params && req.params.locationid) {
+		Loc.findById(req.params.locationid).exec(function (err, location) {
+			if (!location) {
+				sendJsonResponse(res, 404, {
+					"message": "locationid not found"
+				});
+				return;
+			} else if (err) {
+				sendJsonResponse(res, 404, err);
+			}
+			sendJsonResponse(res, 200, location);
+		})
+	} else {
+		sendJsonResponse(res, 404, {
+			"message": "No locationid in request"
+		});
+	}
+	
+};
+module.exports.locationsUpdateOne = function (req, res) {
+	sendJsonResponse(res, 200, {"status": "success"});
+};
+module.exports.locationsDeleteOne = function (req, res) {
+	sendJsonResponse(res, 200, {"status": "success"});
+};
